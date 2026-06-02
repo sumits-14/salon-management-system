c58961f75174d15c94c760b62ceb02621ae01264
sumits-14 <sumitshirdhone07@gmail.com>
2026-06-03 01:19:55 +0530

    Setup Express server and MongoDB connection

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+     res.json({
+          success : true,
+          message : "Salon Management app running!"
+     })
+})
+
+
+export default app
