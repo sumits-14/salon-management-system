3e9367b2244026202bd707b60761e0d3199b12e4
sumits-14 <sumitshirdhone07@gmail.com>
2026-06-15 00:15:44 +0530

    Setup React frontend with Vite and Bootstrap

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>
+      Salon Managment System!
+    </h1>
+  )
+}
+
+export default App
---
+++ frontend/src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import  ReactDom from 'react-dom/client'
+import App from './App.jsx'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+ReactDom.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
